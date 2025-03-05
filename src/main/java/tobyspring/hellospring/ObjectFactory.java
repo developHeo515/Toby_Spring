829fd93d6920964ded4082c70ae9ebb2a2dd5983
허준수 <gjwnstn515@naver.com>
2025-03-06 05:20:44 +0900

    section3: 완료

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,21 +1,27 @@
 package tobyspring.hellospring;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.PaymentService;
 
 @Configuration
-@ComponentScan
 public class ObjectFactory {
-//    @Bean
-//    public PaymentService paymentService() {
-//        return new PaymentService(exRateProvider());
-//    }
-//
-//    @Bean
-//    public ExRateProvider exRateProvider() {
-//        return new SimpleExRateProvider();
-//    }
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(cachedExRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider cachedExRateProvider() {
+        return new CachedExRateProvider(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
 
 }
