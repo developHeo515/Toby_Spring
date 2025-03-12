5752bfaa5df9f42b8612ea2b8e7445a5a4119b07
허준수 <gjwnstn515@naver.com>
2025-03-13 00:45:48 +0900

    section5: ApiTemplate 분리

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,55 +1,22 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.api.*;
 import tobyspring.hellospring.payment.ExRateProvider;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.stream.Collectors;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class WebApiExRateProvider implements ExRateProvider {
+    ApiTemplate apiTemplate = new ApiTemplate();
+
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return runApiForExRate(url);
+        return apiTemplate.getExRate(url, new HttpClientApiExercutor(), new ErApiExRateExtractor());
 
-    }
-
-    private static BigDecimal runApiForExRate(String url) {
-        URI uri;
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-        String response;
-        try {
-            response = new SimpleApiExecutor().execute(uri);
-        } catch (IOException e){
-            throw new RuntimeException(e);
-        }
-
-        try {
-            return extractExRate(response);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static BigDecimal extractExRate(String response) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        ExRateData data = mapper.readValue(response, ExRateData.class);
-        return data.rates().get("KRW");
     }
 
 }
