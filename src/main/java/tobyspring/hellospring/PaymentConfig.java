ecaed87334d8dedefda0612486679b4117957b71
허준수 <gjwnstn515@naver.com>
2025-03-14 07:03:36 +0900

    section: 5 마무리

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,6 +2,12 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import tobyspring.hellospring.api.ApiTemplate;
+import tobyspring.hellospring.api.ErApiExtractor;
+import tobyspring.hellospring.api.SimpleApiExecutor;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
@@ -16,8 +22,13 @@ public class PaymentConfig {
     }
 
     @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate(new JdkClientHttpRequestFactory());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new RestTemplateExRateProvider(restTemplate());
     }
 
     @Bean
